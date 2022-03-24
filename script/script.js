9589db8302308a3c52e1d1855bed103dfa928937
Ajithvc45 <84002008+Ajithvc45@users.noreply.github.com>
2022-03-24 22:12:02 +0530

    validation

---
+++ script/script.js
@@ -0,0 +1,47 @@
+function validate() {
+    let email = document.getElementById("email");
+    let error = document.getElementById("error");
+    let regexp = /^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/
+    if(regexp.test(email.value)){
+        // alert("Valid");
+        error.innerHTML = "Valid Email";
+        error.style.color = "green";
+        return true;
+    }else{
+        // alert("Invalid");
+        error.innerHTML = "Invalid Email";
+        error.style.color = "red";
+        return false;
+    }
+}
+
+let timeout;
+let password = document.getElementById('PassEntry');
+let strengthBadge = document.getElementById('StrengthDisp');
+let strongPassword = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})')
+let mediumPassword = new RegExp('((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))')
+
+
+function StrengthChecker(PasswordParameter) {
+    if(strongPassword.test(PasswordParameter)) {
+        strengthBadge.style.backgroundColor = "green";
+        strengthBadge.textContent = 'Strong';
+    } else if(mediumPassword.test(PasswordParameter)) {
+        strengthBadge.style.backgroundColor = 'blue';
+        strengthBadge.textContent = 'Medium';
+    } else {
+        strengthBadge.style.backgroundColor = 'red';
+        strengthBadge.textContent = 'Weak';
+    }
+}
+
+password.addEventListener("input", () => {
+    strengthBadge.style.display = 'block';
+    clearTimeout(timeout);
+    timeout = setTimeout(() => StrengthChecker(password.value), 500);
+    if(password.value.length !== 0) {
+        strengthBadge.style.display != 'block';
+    } else {
+        strengthBadge.style.display = 'none';
+    }
+});
